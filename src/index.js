c5d87e260de6b1b94a294340360434644facf241
Rohit Kesarwani <rohitk0532@gmail.com>
2017-08-30 21:04:59 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,119 +1,135 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './materialize/dist/css/materialize.min.css';
-
-
-var ws = new WebSocket("ws:stocks.mnet.website");//ws:stocks.mnet.website
-ws.onopen=function(event)
+import 'babel-register';
+class Stocks extends React.Component
 {
-	console.log("connected")
+	
+
+	createStock()
+	{
+		return <Stock name={this.props.name} price={this.props.price}/>
+
+	}
+
+	
+	render()
+	{
+		return (<div> 
+		{this.createStock()}
+		</div>
+
+
+		);
+	}
 }
 
-ws.onmessage=function(event)
+class Stock extends React.Component
 {
-	handleUpdateMessage(event);
+	render()
+	{
+		return (
+
+			<div>
+	
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className=" col s4">{localStorage.key(0)}</div>
+					<div className={localStorage.getItem(localStorage.key(0)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(0)+" time")}</div>
+				</div>
+	
+
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(3)}</div>
+					<div className={localStorage.getItem(localStorage.key(3)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(3))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(3)+" time")}</div>
+				</div>
+
+
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(6)}</div>
+					<div className={localStorage.getItem(localStorage.key(6)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(6)+" time")}</div>
+				</div>
+				
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(9)}</div>
+					<div className={localStorage.getItem(localStorage.key(9)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(9)+" time")}</div>
+				</div>
+
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(12)}</div>
+					<div className={localStorage.getItem(localStorage.key(12)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(12)+" time")}</div>
+				</div>
+
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(15)}</div>
+					<div className={localStorage.getItem(localStorage.key(15)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(15)+" time")}</div>
+				</div>
+	
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(18)}</div>
+					<div className={localStorage.getItem(localStorage.key(18)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(18)+" time")}</div>
+				</div>
+				
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(21)}</div>
+					<div className={localStorage.getItem(localStorage.key(21)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(21)+" time")}</div>
+				</div>
+				
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(24)}</div>
+					<div className={localStorage.getItem(localStorage.key(24)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(24)+" time")}</div>
+				</div>
+			
+				<div className="row items blue lighten-6 white-text  valign-wrap center-align">
+					<div className="col s4">{localStorage.key(27)}</div>
+					<div className={localStorage.getItem(localStorage.key(27)+" color")+" col s4"}>{localStorage.getItem(localStorage.key(0))}</div>
+					<div className="col s4">{localStorage.getItem(localStorage.key(27)+" time")}</div>
+				</div>
+			</div>
+	
+
+	);
+	}
 }
 
-ws.onerror=function(event)
-{
-	console.log(event.data);
-}
+var ws = new WebSocket("ws:stocks.mnet.website");
+var a;
+ws.onmessage=function(event){
 
-class Sts extends React.Component
-{
-
-  render()
-  {
-    return(
-   <StsItems/>
-    );
-  }
-}
-
- class StsItems extends React.Component
- {
-  render() {
-    return (
-      <div>
-
-        <div id="" className="row items blue lighten-6 white-text valign-wrap center-align">
-      <div className="col s4">
-      {localStorage.key(0)}
-      </div>
-      <div className="col s4 green">{localStorage.getItem(localStorage.key(0))}</div>
-      <div className="col s4">{localStorage.getItem(localStorage.key(0)+" time")}</div>
-      </div>
-
- 
-  
-  <div id="" className="row items blue lighten-6 white-text valign-wrap center-align">
-      <div className="col s4">
-      {localStorage.key(2)}
-      </div>
-      <div className={fl===0? "green" +" col s4": "pink" +" col s4" }>{localStorage.getItem(localStorage.key(2))}</div>
-      <div className="col s4">{localStorage.getItem(localStorage.key(2)+" time")}</div>
-      </div>
-
-  <div id="" className="row items blue lighten-6 white-text valign-wrap center-align">
-      <div className="col s4">
-      {localStorage.key(4)}
-      </div>
-      <div className="col s4 green">{localStorage.getItem(localStorage.key(4))}</div>
-      <div className="col s4">{localStorage.getItem(localStorage.key(4)+" time")}</div>
-      </div>
-
-
-<div id="" className="row items blue lighten-6 white-text valign-wrap center-align">
-      <div className="col s4">
-      {localStorage.key(6)}
-      </div>
-      <div className="col s4 green">{localStorage.getItem(localStorage.key(6))}</div>
-      <div className="col s4">{localStorage.getItem(localStorage.key(6)+" time")}</div>
-      </div>
-
-
-
-<div id="" className="row items blue lighten-6 white-text valign-wrap center-align">
-      <div className="col s4">
-      {localStorage.key(8).replace("-time","")}
-      </div>
-      <div className="col s4 green">{localStorage.getItem(localStorage.key(8))}</div>
-      <div className="col s4">{localStorage.getItem(localStorage.key(8)+" time")}</div>
-      </div>
-
-      </div>
-
-      );
-  }
-
- }
-
-
-var a=[],st=[];
-var len,i=0;
- var fl=0;
-
-function handleUpdateMessage(event) 
-{
 	a=JSON.parse(event.data);
+	handleUpdateMessage(a);
 
-  for(i=0;i<a.length;i++)
-    {
-      if(a[i][1]<localStorage.getItem(a[i][0]))
-        fl=1;
-      else
-        fl=0;
-      localStorage.setItem(a[i][0],a[i][1]); 
-      localStorage.setItem(a[i][0]+' time',new Date().toLocaleTimeString()); 
-    }
-    
-
-  len=localStorage.length;
-
-
-  ReactDOM.render(
-    <Sts len={len}/>,
-    document.getElementById('root')
-  );
 }
+
+
+function handleUpdateMessage(data) {
+
+data.forEach(([name, price]) => console.log(name+' '+price));
+data.forEach(([name, price]) => localStorage.setItem(name+" color"," white"));
+data.forEach(([name, price]) => localStorage.setItem(name+" color",(price<Number(localStorage.getItem(name)) ? "pink" : "green")));
+data.forEach(([name, price]) => localStorage.setItem(name,price));
+data.forEach(([name, price]) => localStorage.setItem(name+" time",new Date().toLocaleTimeString()));
+
+
+/*data.forEach(
+
+  ([name, price]) =>ReactDOM.render(<Stocks name={name} price={price}/>,document.getElementById("stocks"))
+
+
+  )*/
+
+  ReactDOM.render(<Stocks/>,document.getElementById("stocks"))
+
+}
+
+
+
+
